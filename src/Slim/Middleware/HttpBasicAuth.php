262152a366c8317665b4b62eb465f913514d976d
Mika Tuupola <tuupola@appelsiini.net>
2013-11-16 22:11:07 +0200

    Initial commit.

---
+++ src/Slim/Middleware/HttpBasicAuth.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Slim\Middleware;
+
+/**
+ * HTTP Basic Authentication
+ *
+ * Provides HTTP Basic Authentication on given routes
+ *
+ * @package    Slim
+ * @author     Mika Tuupola <tuupola@appelsiini.net>
+ */
+class HttpBasicAuth extends \Slim\Middleware {
+
+    public $options;
+
+    public function __construct($options = null) {
+
+        /* Default options. */
+        $this->options = array(
+            "users" => array(),
+            "path" => "/",
+            "real" => "Protected"
+        );
+
+        if ($options) {
+            $this->options = array_merge($this->options, (array)$options);
+        }
+    }
+
+    public function call() {
+        $request = $this->app->request;
+
+        /* If path matches what is given on initialization. */
+        if (false !== strpos($request->getPath(), $this->options["path"])) {
+            $user = $request->headers("PHP_AUTH_USER");
+            $pass = $request->headers("PHP_AUTH_PW");
+
+            /* Check if user and passwords matches. */
+            if (isset($this->options["users"][$user]) && $this->options["users"][$user] === $pass) {
+                $this->next->call();
+            } else {
+                $this->app->response->status(401);
+                $this->app->response->header("WWW-Authenticate", sprintf('Basic realm="%s"', $this->options["realm"]));;
+                return;
+            }
+        } else {
+            $this->next->call();
+        }
+    }
+}
