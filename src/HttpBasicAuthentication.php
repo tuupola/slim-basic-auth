25bd997365043e97b0935189071fd582ee1e85d6
John Porter <johnrobertporter@users.noreply.github.com>
2017-09-18 15:43:19 +0100

    Add user to error args (#46) (fixes #45)

--- src/HttpBasicAuthentication.php
+++ src/HttpBasicAuthentication.php
@@ -126,7 +126,8 @@ class HttpBasicAuthentication
                 ->withHeader("WWW-Authenticate", sprintf('Basic realm="%s"', $this->options["realm"]));
 
             return $this->error($request, $response, [
-                "message" => "Authentication failed"
+                "message" => "Authentication failed",
+                "user" => $user,
             ]);
         }
 
@@ -139,8 +140,9 @@ class HttpBasicAuthentication
                     ->withHeader("WWW-Authenticate", sprintf('Basic realm="%s"', $this->options["realm"]));
 
                 return $this->error($request, $response, [
-                    "message" => "Callback returned false"
-                ]);
+                    "message" => "Callback returned false",
+                    "user" => $user,
+                    ]);
             }
         }
 
